046cc73eaa84a2c25078e5c3cf83a515b535b5be
sagarde007 <sagarde2004@gmail.com>
2024-09-21 23:03:38 +0530

    Array

---
+++ Arrays/printArr.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include<iostream>
+using namespace std;
+    
+int main(){
+    int arr[] = {1,42,12,2,3};
+    int n = 5;
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
